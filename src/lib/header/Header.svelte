562b5043ddf4a69a100ff76332efb73c9569dafb
Matthias Hollerbach <matzehollerbach@web.de>
2022-07-20 11:40:39 +0200

    add header

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,102 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<div class="logo">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/portfolio'}>
+				<a sveltekit:prefetch href="/portfolio">portfolio</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		position: sticky;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 100;
+		backdrop-filter: blur(5px);
+		display: flex;
+		justify-content: space-between;
+		--background: rgba(255, 255, 255, 0.8);
+		background: var(--background);
+	}
+
+	.logo a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.logo img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: right;
+	}
+
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent-color);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Header from "$lib/header/Header.svelte";
   import "../app.css";
 </script>
 
+<Header></Header>
 <div class="container mx-auto">
   <slot />
 </div>
